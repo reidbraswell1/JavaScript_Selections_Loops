51b52716cc0f3f06ade07e79ac274542c5ef015c
reid <reidbras@gmail.com>
2021-06-30 22:20:30 -0500

    Updating exercise

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+for(let i=1; i <=100; i++) {
+    if(i % 2 == 0) {
+        let text = document.createTextNode(i);
+        let br = document.createElement("br");
+        document.getElementById("exercise-1").appendChild(text);
+        document.getElementById("exercise-1").appendChild(br);
+    }
+}
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
